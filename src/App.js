233d604b680501217da96c87deb8b5f2821c244b
Hanna <hanna.svintsitska@gmail.com>
2023-11-27 17:34:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Кіберполіція та податкова дуже зацікавилась вашою компанією. Шкода, що
+          немає назви компанії, проте вашого номеру теж буде достатньо для
+          заявки)
         </p>
         <a
           className="App-link"
